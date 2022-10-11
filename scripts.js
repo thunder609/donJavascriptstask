b3f7ef6618601ee16518bee056c0c63df8936d96
thunder609 <oswaldo-pachco@hotmail.es>
2022-10-11 01:51:24 -0400

    clase 03 terminada

--- scripts.js
+++ scripts.js
@@ -2,7 +2,21 @@ const btn = document.querySelector('[data-for-btn]');
 const Task = (e)=>{
     e.preventDefault()
     const input = document.querySelector('[data-for-input]');
-    console.log(input.value)   ;
-}
+    const value = input.value;
+    input.value = ""
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement('li')
+    task.classList.add("card")
+    console.log(value) ;
+    const content = `  <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>
+</li>
+`;
+task.innerHTML=content
+list.appendChild(task)
+} 
 //Listener
-btn.addEventListener(click,Task)
+btn.addEventListener('click',Task)
